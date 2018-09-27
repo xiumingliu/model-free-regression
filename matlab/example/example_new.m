bce742a2ff5b48e2499ff84207807395c2c630d1
Xiuming Liu <xiuming.liu@it.uu.se>
2018-09-27 15:34:39 +0200

    figures

--- matlab/example/example_new.m
+++ matlab/example/example_new.m
@@ -422,8 +422,8 @@ contour(X1_test_EP, X2_test_EP, y_predict_EP, 'LevelList', [0.2], 'LineWidth', 3
 xlabel('$x_1$', 'Interpreter', 'latex');
 ylabel('$x_2$', 'Interpreter', 'latex');
 set(gca, 'FontSize', 18, 'FontWeight', 'bold')
-saveas(gcf, fullfile(fpath, 'uncertatinty.png'));
-saveas(gcf, fullfile(fpath, 'uncertatinty.fig'));
+saveas(gcf, fullfile(fpath, 'uncertainty2.png'));
+saveas(gcf, fullfile(fpath, 'uncertainty2.fig'));
 
 %% The Example
 % MCAR
@@ -444,10 +444,10 @@ scatter(X_unlabeled((y_unlabeled==1), 1), X_unlabeled((y_unlabeled==1), 2), 50, 
 scatter(X_labeled((y_labeled==0),1), X_labeled((y_labeled==0),2), 100, 'or', 'LineWidth', 3);
 scatter(X_labeled((y_labeled==1),1), X_labeled((y_labeled==1),2), 100, 'xb', 'LineWidth', 3);
 fcontour(@(x1, x2)(p_y_1*pdf(p_xy_1, [x1 x2]) - p_y_0*pdf(p_xy_0, [x1 x2])),...
-    [-5 5 -5 5], 'LevelList', [0], 'LineWidth', 3, 'LineStyle', '--')
+    [-5 5 -5 5], 'LevelList', [0], 'LineWidth', 3, 'LineStyle', '--', 'LineColor', 'k')
 fcontour(@(x1, x2)(p_y_1*mvnpdf([x1 x2], p_xy_1_mcar.m, p_xy_1_mcar.cov) - ...
     p_y_0*mvnpdf([x1 x2], p_xy_0_mcar.m, p_xy_0_mcar.cov)), [-5 5 -5 5], ...
-    'LevelList', [0], 'LineWidth', 3, 'LineStyle', ':')
+    'LevelList', [0], 'LineWidth', 3, 'LineStyle', ':', 'LineColor', 'k')
 
 lgd = legend({'Labeled, class 0', 'Labeled, class 1', 'Unlabeled, class 0', ...
     'Unlabeled, class 1', 'Naive Bayesian CC', 'Naive Bayesian SC'}, ...
