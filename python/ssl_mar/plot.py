b6997c63784cedd8b58138698ca32784049864a9
Xiuming Liu <xiuming.liu@it.uu.se>
2019-01-18 12:46:21 +0100

    new figures

--- python/ssl_mar/plot.py
+++ python/ssl_mar/plot.py
@@ -58,8 +58,8 @@ def plot_digits(decoder):
     
 def plot_labeled_unlabeled(z_labeled, y_labeled, z_unlabeled):
     plt.figure(figsize=(6, 5))
-    plt.scatter(z_unlabeled[:, 0], z_unlabeled[:, 1], marker='+', c=(0,0,0), alpha=0.05)
-    plt.scatter(z_labeled[:, 0], z_labeled[:, 1], marker='+', c=y_labeled, cmap=cmap, vmin = -0.5, vmax = 9.5)
+    plt.scatter(z_unlabeled[:, 0], z_unlabeled[:, 1], marker='x', c=(0,0,0), alpha=0.05)
+    plt.scatter(z_labeled[:, 0], z_labeled[:, 1], marker='x', c=y_labeled, cmap=cmap, vmin = -0.5, vmax = 9.5)
     plt.xlabel("$z_1$")
     plt.ylabel("$z_2$")
     plt.xlim([-6, 6])
@@ -70,7 +70,7 @@ def plot_labeled_unlabeled(z_labeled, y_labeled, z_unlabeled):
     
 def plot_maxlrt(z_unlabeled, lrt_y): 
     plt.figure(figsize=(6, 5))
-    plt.scatter(z_unlabeled[:, 0], z_unlabeled[:, 1], c=np.max(lrt_y, axis=1), marker='+', cmap='jet', vmin = -5, vmax = 5)
+    plt.scatter(z_unlabeled[:, 0], z_unlabeled[:, 1], c=np.max(lrt_y, axis=1), marker='x', cmap='jet', vmin = -5, vmax = 5)
     plt.xlabel("$z_1$")
     plt.ylabel("$z_2$")
     plt.xlim([-6, 6])
@@ -123,7 +123,7 @@ def plot_test(z_test, y_test_hat, y_test, pe_test):
     
     
     plt.figure(figsize=(6, 5))
-    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='+', alpha=0.1, cmap='jet', vmin = 0, vmax = .9)
+    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='x', alpha=0.1, cmap='jet', vmin = 0, vmax = .9)
     plt.colorbar()
     plt.xlabel("$z_1$")
     plt.ylabel("$z_2$")
@@ -154,7 +154,7 @@ def plot_test_mcar(z_test, y_test_hat, y_test, pe_test, leftout_classes):
     index_errors_2 = np.nonzero(np.logical_and(y_test != y_test_hat, np.logical_not(np.isin(y_test, leftout_classes))))    
     
     plt.figure(figsize=(6, 5))
-    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='+', cmap='jet', vmin = 0, vmax = .9)
+    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='x', cmap='jet', vmin = 0, vmax = .9)
     plt.colorbar()
     plt.xlabel("$z_1$")
     plt.ylabel("$z_2$")
@@ -177,7 +177,7 @@ def plot_test_mcar_soft(z_test, y_test_hat, y_test, pe_test, leftout_classes):
     index_errors_2 = np.nonzero(np.logical_and(y_test != y_test_hat, np.logical_not(np.isin(y_test, leftout_classes))))    
     
     plt.figure(figsize=(6, 5))
-    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='+', cmap='jet', vmin = 0, vmax = .9)
+    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='x', cmap='jet', vmin = 0, vmax = .9)
     plt.colorbar()
     plt.xlabel("$z_1$")
     plt.ylabel("$z_2$")
@@ -200,7 +200,7 @@ def plot_test_mar(z_test, y_test_hat, y_test, pe_test, leftout_classes):
     index_errors_2 = np.nonzero(np.logical_and(y_test != y_test_hat, np.logical_not(np.isin(y_test, leftout_classes))))    
     
     plt.figure(figsize=(6, 5))
-    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='+', cmap='jet', vmin = 0, vmax = .9)
+    plt.scatter(z_test[:, 0], z_test[:, 1], c=pe_test, marker='x', cmap='jet', vmin = 0, vmax = .9)
     plt.colorbar()
     plt.xlabel("$z_1$")
     plt.ylabel("$z_2$")
